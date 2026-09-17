5c51a3a6575f47eb199cd9e811b98a63ef3bfdbd
jivan subedi <subedijiwan5@gmail.com>
2026-09-17 12:32:09 +0545

    fixedddd

--- chats/views.py
+++ chats/views.py
@@ -1344,7 +1344,8 @@ class StartCallView(APIView):
         )
 
         sent = 0
-
+        print("items:"+item)
+        print("it is a item")
         for item in tokens.iterator():
             print("actually called things")
             try:
